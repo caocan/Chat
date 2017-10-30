7ba542a8c991922f351e4ad1231cd0861697f817
caocan <wwwcaocan@sina.cn>
2017-10-30 10:59:18 +0800

    Chat0.3版本，添加窗口关闭机制

--- Chat0.3/src/ChatClient.java
+++ Chat0.3/src/ChatClient.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ChatClient extends Frame{
 
@@ -15,6 +17,12 @@ public class ChatClient extends Frame{
         add(tfTxt, BorderLayout.SOUTH); //添加显示框
         add(taContent, BorderLayout.NORTH); //添加输入框
         pack();  //这个方法就是依据你放置的组件设定窗口的大小 使之正好能容纳你放置的所有组件
+        this.addWindowListener(new WindowAdapter() {    //添加窗口关闭事件
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
         setVisible(true);
     }
 }
